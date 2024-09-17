a69796511d096ab55cfa11fd36720d7d989991dd
Prince Eklu <ameto888@protonmail.com>
2024-09-17 09:52:41 -0400

    Add Product and store data globaly

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import './ProductList.css'; 
+// import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItemToCart } from './CartSlice';
+import './ProductList.css'; // Import CSS file for component-specific styles
 
 const ProductList = () => {
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
+  };
 
   return (
     <div className="product-list">
@@ -16,7 +26,11 @@ const ProductList = () => {
     {products.map(product => (
       <li key={product.id} className="product-list-item">
       <span>{product.name} - ${product.price}</span>
-      <button>
+      <button
+      className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+      onClick={() => handleAddToCart(product)}
+      disabled={disabledProducts.includes(product.id)} // Disable button if product is in disabledProducts
+      >
       Add to Cart
       </button>
       </li>
@@ -25,49 +39,5 @@ const ProductList = () => {
     </div>
   );
 };
-
-
-
-
-
-
-
-
-/*
-return (
-  <div className="product-list">
-  <h2 className="product-list-title">Products</h2>
-  <ul className="product-list-items">
-  {products.map(product => (
-    <li key={product.id} className="product-list-item">
-    <span>{product.name} - ${product.price}</span>
-    <button
-    className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
-    onClick={() => handleAddToCart(product)}
-    disabled={disabledProducts.includes(product.id)} // Disable button if product is in disabledProducts
-    >
-    Add to Cart
-    </button>
-    </li>
-  ))}
-  </ul>
-  </div>
-);
-};*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default ProductList;
